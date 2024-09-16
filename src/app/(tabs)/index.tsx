10b0913c0d5239a723f6c1e0cd0cd5eb66244637
braswelljr <braswellkenneth7@gmail.com>
2024-09-16 13:06:22 +0000

    chore: fix imports and file names

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -8,7 +8,7 @@ export default function HomeScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={<Image source={require('@/assets/images/partial-react-logo.png')} style={styles.reactLogo} />}
+      headerImage={<Image source={require('~/assets/images/partial-react-logo.png')} style={styles.reactLogo} />}
     >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
